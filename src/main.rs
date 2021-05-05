a37cc92686435d2416589c05d8a338625bf78189
Zixun Yu <yu645@purdue.edu>
2021-05-05 10:59:26 -0400

    Use multithread

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod bvh;
 mod camera;
 mod hcm;
 mod image;
@@ -6,22 +7,21 @@ mod material;
 mod ray;
 mod shape;
 
+use rayon::prelude::*;
 use std::fs::File;
 use std::io::{self, BufWriter};
 use std::path::Path;
+use std::sync::Arc;
+use std::time::{Instant};
 
 use hcm::{Point3, Vec3};
 use image::Color;
 use instance::Instance;
 use io::Write;
 use material::{Dielectric, Lambertian, Metal};
-use ray::Ray;
-use shape::Shape;
 
 const WIDTH: u32 = 1200;
 const HEIGHT: u32 = 800;
-const NUM_CHANNELS: u32 = 4;
-const NUM_BYTES: usize = (WIDTH * HEIGHT * NUM_CHANNELS) as usize;
 const MSAA: usize = 12;
 const SAMPLES_PER_PIXEL: usize = MSAA * MSAA;
 
@@ -34,7 +34,7 @@ fn radiance_in(scene: &[Instance], mut ray: ray::Ray, depth: i32) -> Color {
     }
 
     let mut closest_hit = None;
-    let mut mtl = scene[0].mtl;
+    let mut mtl = &scene[0].mtl;
     for inst in scene.iter() {
         let isect = inst.shape.intersect(&ray);
         match isect {
@@ -42,7 +42,7 @@ fn radiance_in(scene: &[Instance], mut ray: ray::Ray, depth: i32) -> Color {
             Some(hit) => {
                 ray.set_extent(hit.ray_t);
                 closest_hit = isect;
-                mtl = inst.mtl;
+                mtl = &inst.mtl;
             }
         }
     }
@@ -75,14 +75,14 @@ fn rand_f32() -> f32 {
 }
 
 pub fn write_image(file_name: &str, data: &[u8], (width, height): (u32, u32)) {
-    assert_eq!(data.len(), (width * height * 4) as usize);
+    assert_eq!(data.len(), (width * height * 3) as usize);
 
     let path = Path::new(file_name);
     let file = File::create(path).unwrap();
     let ref mut w = BufWriter::new(file);
 
     let mut encoder = png::Encoder::new(w, width, height);
-    encoder.set_color(png::ColorType::RGBA);
+    encoder.set_color(png::ColorType::RGB);
     encoder.set_depth(png::BitDepth::Eight);
     let mut writer = encoder.write_header().unwrap();
 
@@ -109,11 +109,11 @@ fn main() {
         shape::Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0),
     ];
 
-    let mut mtls: Vec<Box<dyn material::Material>> = vec![
-        Box::new(Lambertian::new(Color::new(0.5, 0.5, 0.5))),
-        Box::new(Dielectric::new(1.5)),
-        Box::new(Lambertian::new(Color::new(0.4, 0.2, 0.1))),
-        Box::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0)),
+    let mut mtls: Vec<Arc<dyn material::Material>> = vec![
+        Arc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5))),
+        Arc::new(Dielectric::new(1.5)),
+        Arc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1))),
+        Arc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0)),
     ];
 
     for a in -11..11 {
@@ -124,77 +124,95 @@ fn main() {
 
             if (center - Point3::new(4.0, 0.2, 0.0)).norm() > 0.9 {
                 spheres.push(shape::Sphere::new(center, 0.2));
-                let mtl: Box<dyn material::Material> = if choose_mtl < 0.8 {
+                let mtl: Arc<dyn material::Material> = if choose_mtl < 0.8 {
                     let albedo = Color::new(rand_f32(), rand_f32(), rand_f32());
-                    Box::new(Lambertian::new(albedo))
+                    Arc::new(Lambertian::new(albedo))
                 } else if choose_mtl < 0.95 {
                     let albedo = Color::new(rand_f32(), rand_f32(), rand_f32());
                     let albedo = (albedo + Color::white()) * 0.5;
-                    Box::new(Metal::new(albedo, rand_f32() * 0.5))
+                    Arc::new(Metal::new(albedo, rand_f32() * 0.5))
                 } else {
-                    Box::new(Dielectric::new(1.4))
+                    Arc::new(Dielectric::new(1.4))
                 };
                 mtls.push(mtl);
             }
         }
     }
 
+    let spheres: Vec<_> = spheres.into_iter().map(|s| Arc::new(s)).collect();
+
     let instances: Vec<Instance> = mtls
-        .iter()
-        .zip(spheres.iter())
-        .map(|(mtl, sphere)| Instance::new(sphere, &**mtl))
+        .into_iter()
+        .zip(spheres.into_iter())
+        .map(|(mtl, sphere)| Instance::new(sphere, mtl))
         .collect();
 
-    let mut data: Vec<u8> = Vec::new();
-    data.resize(NUM_BYTES, 0);
+    let start_render = Instant::now();
 
-    let mut jitters = vec![];
-    for row in 0..HEIGHT {
-        if (row % 10) == 0 {
-            print!("{} ", row);
-            io::stdout().flush().unwrap();
-        }
-        for col in 0..WIDTH {
-            let mut color_sum = Color::black();
-
-            for i in 0..MSAA * MSAA {
-                let jitter = (
-                    ((i / MSAA) as f32 + rand::random::<f32>()) / MSAA as f32,
-                    ((i % MSAA) as f32 + rand::random::<f32>()) / MSAA as f32,
-                );
-                let jitter = (rand::random::<f32>(), rand::random::<f32>());
-                jitters.push(jitter);
-                let ray = camera.shoot_ray(row, col, jitter).unwrap();
-                color_sum = color_sum + radiance_in(&instances, ray, 50);
+    let color_data: Vec<_> = (0..HEIGHT)
+        .into_par_iter()
+        .map(|row| {
+            if (row % 10) == 0 {
+                print!("{} ", row);
+                io::stdout().flush().unwrap();
             }
-            let color = (color_sum / (SAMPLES_PER_PIXEL as f32))
-                .gamma_encode()
-                .to_u8();
+            let mut colors_for_row = vec![];
+            for col in 0..WIDTH {
+                let mut color_sum = Color::black();
 
-            let base = (row * WIDTH + col) as usize * 4;
-            data[base + 0] = color[0];
-            data[base + 1] = color[1];
-            data[base + 2] = color[2];
-            data[base + 3] = 255;
-        }
-    }
+                for i in 0..MSAA * MSAA {
+                    let jitter = (
+                        ((i / MSAA) as f32 + rand::random::<f32>()) / MSAA as f32,
+                        ((i % MSAA) as f32 + rand::random::<f32>()) / MSAA as f32,
+                    );
+                    let jitter = (rand::random::<f32>(), rand::random::<f32>());
+                    let ray = camera.shoot_ray(row, col, jitter).unwrap();
+                    color_sum = color_sum + radiance_in(&instances, ray, 50);
+                }
 
-    write_image(r"output.png", &data, (WIDTH, HEIGHT));
+                let color = color_sum / (SAMPLES_PER_PIXEL as f32);
+                colors_for_row.push(color);
+            }
+            colors_for_row
+        })
+        .collect();
 
-    drop(data);
+    // let mut image_map = vec![];
+    // for mut row in color_data.into_iter() {
+    //     image_map.append(&mut row);
+    // }
 
-    let mut data: Vec<u8> = Vec::new();
-    data.resize(256 * 256 * 4 as usize, 230);
-    for (dx, dy) in jitters.iter().take(10000) {
-        let ix = (dx * 192.0) as usize + 32;
-        let iy = (dy * 192.0) as usize + 32;
+    let image_map = color_data
+        .into_iter()
+        .fold(vec![], |mut partial_image, mut row| {
+            partial_image.append(&mut row);
+            partial_image
+        });
+    let image_data = image_map
+        .iter()
+        .fold(Vec::<u8>::new(), |mut partial_data, color| {
+            partial_data.append(&mut color.gamma_encode().to_u8().to_vec());
+            partial_data
+        });
 
-        let base = (iy * 256 + ix) * 4;
-        data[base + 0] = 30;
-        data[base + 1] = 30;
-        data[base + 2] = 30;
-        data[base + 3] = 255;
-    }
+    let whole_render_time = Instant::now().duration_since(start_render);
 
-    write_image(r"samples.png", &data, (256, 256));
+    println!("whole render time = {} us", whole_render_time.as_micros());
+
+    write_image(r"output.png", &image_data, (WIDTH, HEIGHT));
+
+    // let mut data: Vec<u8> = Vec::new();
+    // data.resize(256 * 256 * 4 as usize, 230);
+    // for (dx, dy) in jitters.iter().take(10000) {
+    //     let ix = (dx * 192.0) as usize + 32;
+    //     let iy = (dy * 192.0) as usize + 32;
+
+    //     let base = (iy * 256 + ix) * 4;
+    //     data[base + 0] = 30;
+    //     data[base + 1] = 30;
+    //     data[base + 2] = 30;
+    //     data[base + 3] = 255;
+    // }
+
+    // write_image(r"samples.png", &data, (256, 256));
 }
