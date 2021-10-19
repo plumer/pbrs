f2205d8f5a3e707bdd49ed8b2f46f27d32b8a18b
Zixun Yu <yzx19942013@gmail.com>
2021-10-18 23:08:51 -0400

    Major change to Light interface
    - Removes AreaLight trait as the main implementation is
      DiffuseAreaLight.
    - radiance_from(&Interaction, Vec3) moves to DiffuseAreaLight.
    - Adds DiffuseAreaLight::radiance_to(&Interaction, wi).
    - Adds DiffuseAreaLight::pdf_li(&Interaction, wi).
    - ShapeSample::pdf_at() now returns Option<f32> for out-of-domain
      x-values.
    - Implements ShapeSample for QuadXY :)

--- src/light.rs
+++ src/light.rs
@@ -1,35 +1,28 @@
-use std::f32::consts;
+use std::f32::consts::{self, PI};
 
+use geometry::ray::Ray;
 use math::hcm;
 use math::prob::Prob;
 use radiometry::color::Color;
-use crate::ray::Ray;
-use crate::shape::{self, Interaction, Shape};
+use shape::{self, Interaction, Shape};
+
+pub type EnvLight = fn(Ray) -> Color;
 
 fn spawn_ray_to(p0: &Interaction, p1: hcm::Point3) -> Ray {
-    let r = p0.spawn_ray(p1 - p0.pos);
+    let mut r = p0.spawn_ray(p1 - p0.pos);
     r.t_max = p1.distance_to(p0.pos) / r.dir.norm_squared();
     r
 }
 
 pub trait Light {
-    /// Computes the radiance that the light emits to a given point assuming no occlusion.
+    /// Computes the radiance that the light emits to a given point regardless of occlusion.
     /// - Returns the incident direction, the probability that the incident direction gets sampled,
     ///   and the ray to test the visibility.
     fn sample_incident_radiance(
-        &self,
-        target: &Interaction,
-        u: (f32, f32),
+        &self, target: &Interaction, u: (f32, f32),
     ) -> (Color, hcm::Vec3, Prob, Ray);
 
     fn power(&self) -> Color;
-}
-
-pub trait AreaLight: Light {
-    /// Evaluates the area light's emitted radiance in the given outgoing direction.
-    /// Parameters:
-    /// - `from` is the point being evaluated, assumed to be on the surface.
-    fn radiance_from(&self, from: &Interaction, wo: hcm::Vec3) -> Color;
 }
 
 // Various kinds of lights.
@@ -41,9 +34,7 @@ struct PointLight {
 
 impl Light for PointLight {
     fn sample_incident_radiance(
-        &self,
-        target: &Interaction,
-        _u: (f32, f32),
+        &self, target: &Interaction, _u: (f32, f32),
     ) -> (Color, hcm::Vec3, Prob, Ray) {
         let radiance = self.intensity / self.position.squared_distance_to(target.pos);
         let wi = (self.position - target.pos).hat();
@@ -52,7 +43,7 @@ impl Light for PointLight {
     }
 
     fn power(&self) -> Color {
-        self.intensity
+        self.intensity * 4.0 * PI
     }
 }
 
@@ -65,9 +56,7 @@ pub struct DistantLight {
 
 impl Light for DistantLight {
     fn sample_incident_radiance(
-        &self,
-        target: &Interaction,
-        _u: (f32, f32),
+        &self, target: &Interaction, _u: (f32, f32),
     ) -> (Color, hcm::Vec3, Prob, Ray) {
         let outside_world = target.pos + self.world_radius * 2.0 * self.incident_direction;
         let visibility_ray = spawn_ray_to(target, outside_world);
@@ -102,17 +91,15 @@ pub trait ShapeSample: Shape {
     fn sample_towards(&self, target: &Interaction, rnd2: (f32, f32)) -> Interaction;
 
     /// PDF of `wi` lies in the solid angle that this shape casts onto the reference point.
+    /// Returns `None` if the shape doesn't cover the reference point at angle `wi`.
     ///
     /// Assuming the shape is fairly far away from the reference point, the solid angle is
-    /// distance divided by perpendicular area.
-    fn pdf_at(&self, reference: &Interaction, wi: hcm::Vec3) -> f32 {
-        let ray = Ray::new(reference.pos, wi);
-        match self.intersect(&ray) {
-            None => 0.0,
-            Some(hit) => {
-                reference.pos.distance_to(hit.pos) / (hit.normal.dot(-wi).abs() * self.area())
-            }
-        }
+    /// perpendicular area of the shape divided by distance.
+    fn pdf_at(&self, reference: &Interaction, wi: hcm::Vec3) -> Option<f32> {
+        let ray = reference.spawn_ray(wi);
+        let hit = self.intersect(&ray)?;
+        let pdf = reference.pos.distance_to(hit.pos) / (hit.normal.dot(-wi).abs() * self.area());
+        Some(pdf)
     }
 
     fn area(&self) -> f32;
@@ -129,20 +116,58 @@ pub struct DiffuseAreaLight {
 impl DiffuseAreaLight {
     pub fn new(emit_radiance: Color, shape: Box<dyn ShapeSample>) -> Self {
         let area = shape.area();
-        Self{emit_radiance, shape, area}
+        Self {
+            emit_radiance,
+            shape,
+            area,
+        }
+    }
+
+    /// Evaluates the area light's emitted radiance in the given outgoing direction.
+    /// Parameters:
+    ///  - `from` is the point being evaluated, assumed to be on the surface.
+    /// Corresponds to `AreaLight::L(Interaction, Vec3)` in C++ version.
+    fn radiance_from(&self, from: &Interaction, wo: hcm::Vec3) -> Color {
+        if from.normal.dot(wo).is_sign_positive() {
+            self.emit_radiance
+        } else {
+            Color::black()
+        }
+    }
+
+    /// Computes the radiance along direction `wi` towards `target`.
+    ///
+    /// Returns `None` if the ray `target` + t`wi` doesn't hit the underlying shape of the area
+    /// light (regardless of occlusion).
+    /// Otherwise, returns the incident radiance, together with the PDF that `wi` is covered, and
+    /// a ray to test the visibility from `target` to the light shape.
+    pub fn radiance_to(&self, target: &Interaction, wi: hcm::Vec3) -> Option<(Color, f32, Ray)> {
+        let light_hit = self.shape.intersect(&target.spawn_ray(wi))?;
+        let light_pdf = self.pdf_li(target, wi)?;
+        Some((
+            self.emit_radiance,
+            light_pdf,
+            target.spawn_limited_ray_to(light_hit.pos),
+        ))
+    }
+    /// Computes the probability density that the `target` intersection is illuminated by the
+    /// light at angle `wi`.
+    pub fn pdf_li(&self, target: &Interaction, wi: hcm::Vec3) -> Option<f32> {
+        self.shape.pdf_at(target, wi)
     }
 }
 
 impl Light for DiffuseAreaLight {
+    /// Computes the radiance that the light emits to a given point regardless of occlusion.
+    /// - Returns the incident direction, the probability that the incident direction gets sampled,
+    ///   and the ray to test the visibility.
     fn sample_incident_radiance(
-        &self,
-        target: &Interaction,
-        u: (f32, f32),
+        &self, target: &Interaction, u: (f32, f32),
     ) -> (Color, hcm::Vec3, Prob, Ray) {
         let point_on_light = self.shape.sample_towards(target, u);
         let wi = (point_on_light.pos - target.pos).hat();
         let radiance = self.radiance_from(target, -wi);
-        let pdf = self.shape.pdf_at(target, wi);
+        let pdf = self.shape.pdf_at(target, wi).expect("Shoul be nonzero");
         (
             radiance,
             wi,
@@ -150,18 +175,9 @@ impl Light for DiffuseAreaLight {
             spawn_ray_to(target, point_on_light.pos),
         )
     }
+
     fn power(&self) -> Color {
         self.emit_radiance * self.area * std::f32::consts::PI
-    }
-}
-
-impl AreaLight for DiffuseAreaLight {
-    fn radiance_from(&self, from: &Interaction, wo: hcm::Vec3) -> Color {
-        if from.normal.dot(wo).is_sign_positive() {
-            self.emit_radiance
-        } else {
-            Color::black()
-        }
     }
 }
 
@@ -217,22 +233,17 @@ impl ShapeSample for shape::Sphere {
         self.radius().powi(2) * 4.0 * std::f32::consts::PI
     }
 
-    fn pdf_at(&self, reference: &Interaction, wi: hcm::Vec3) -> f32 {
+    fn pdf_at(&self, reference: &Interaction, wi: hcm::Vec3) -> Option<f32> {
         let ref_to_center = self.center() - reference.pos;
         if ref_to_center.norm_squared() < self.radius().powi(2) {
-            1.0 / self.area()
+            Some(1.0 / self.area())
         } else {
             let sin_theta_max_2 = self.radius().powi(2) / ref_to_center.norm_squared();
             let cos_theta_max = (1.0 - sin_theta_max_2).max(0.0).sqrt();
             let cos_theta = ref_to_center.dot(wi) / (ref_to_center.norm() * wi.norm());
-            let uniform_cone_pdf =
-                |cos_theta_max| 1.0 / (2.0 * std::f32::consts::PI * (1.0 - cos_theta_max));
-            if cos_theta > cos_theta_max {
-                // Theta is less than theta_max, `wi` inside the cone
-                uniform_cone_pdf(cos_theta_max)
-            } else {
-                0.0
-            }
+            let uniform_cone_pdf = || 1.0 / (2.0 * std::f32::consts::PI * (1.0 - cos_theta_max));
+            // Theta is less than theta_max, `wi` inside the cone
+            (cos_theta > cos_theta_max).then(uniform_cone_pdf)
         }
     }
 }
@@ -254,5 +265,25 @@ impl ShapeSample for shape::IsolatedTriangle {
     }
     fn area(&self) -> f32 {
         (self.p0 - self.p1).cross(self.p2 - self.p1).norm() * 0.5
+    }
+}
+
+impl ShapeSample for shape::QuadXY {
+    fn sample(&self, rnd2: (f32, f32)) -> Interaction {
+        let (u, v) = rnd2;
+        let pos = hcm::Point3::new(self.x_interval.lerp(u), self.y_interval.lerp(v), self.z);
+        let normal = hcm::Vec3::zbase();
+        Interaction::rayless(pos, rnd2, normal)
+    }
+
+    fn sample_towards(&self, target: &Interaction, rnd2: (f32, f32)) -> Interaction {
+        let mut temp = self.sample(rnd2);
+        let forward = target.pos - temp.pos;
+        temp.normal = forward.dot(temp.normal).signum() * temp.normal;
+        temp
+    }
+
+    fn area(&self) -> f32 {
+        self.x_interval.length() * self.y_interval.length()
     }
 }
